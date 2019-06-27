8b46aaa800d4f13bedba20cdfe368591a95d559f
Maxim <maxsateon@gmail.com>
2019-06-27 18:07:34 +0300

    add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
-// Slider
-
 $(document).ready(function(){
+    // slider
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -8,4 +7,25 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="img/arrow_right.png"></button>',
         
     });
-  });
+
+    // tabs
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+    // link
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
